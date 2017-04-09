3cca637b8ae320a820892203c1425b87f029b905
Robert Boivin <poivronjaune@gmail.com>
2017-04-09 01:53:58 -0400

    Code improvements using JSON instead of XML
    Changed the transformation logic to use JSON manipulation rather than
    XML manipulation. Used json.dumps() to produce the new line delimited
    JSONL file. Added SEAO February 2017 and SEAO March 2017 publisged open
    data.

--- seao.py
+++ seao.py
@@ -3,8 +3,10 @@
 Created on Fri Mar 31 21:05:48 2017
 """
 import codecs
+import xmltodict
+import json
+import xml.etree.ElementTree as et
 
-import seao_common as seao
 
 def saveJsonFile(loadedXMLFile,JSonToSave_str):
     slash = loadedXMLFile.find("\\")
@@ -14,27 +16,122 @@ def saveJsonFile(loadedXMLFile,JSonToSave_str):
     file.close()
     print("- JSON File saved as : " + outfile)
 
+def InitDataPaths():
+    Avis = []
+    Avis.append('2017-03\Avis_20170301_20170331.xml')
+    Avis.append('2017-02\Avis_20170201_20170228.xml')
+    Avis.append('2017-01\Avis_20170101_20170131.xml')
+    Avis.append('2016-12\Avis_20161201_20161231.xml')
+    Avis.append('2016-11\Avis_20161101_20161130.xml')
+    Avis.append('2016-10\Avis_20161001_20161031.xml')
+    Avis.append('2016-09\Avis_20160901_20160930.xml')
+    Avis.append('2016-08\Avis_20160801_20160831.xml')
+    Avis.append('2016-07\Avis_20160701_20160731.xml')
+    Avis.append('2016-06\Avis_20160601_20160630.xml')
+    Avis.append('2016-05\Avis_20160501_20160531.xml')
+    Avis.append('2016-04\Avis_20160401_20160430.xml')
+    Avis.append('2016-03\Avis_20160301_20160331.xml')
+    Avis.append('2016-02\Avis_20160201_20160229.xml')
+    Avis.append('2016-01\Avis_20160101_20160131.xml')
+    Avis.append('2015\Avis_20150101_20151231.xml')
+    Avis.append('2014\Avis_20140101_20141231.xml')
+    Avis.append('2013\Avis_20130101_20131231.xml')
+    Avis.append('2012\Avis_20120101_20121231.xml')
+    Avis.append('2011\Avis_20110101_20111231.xml')
+    Avis.append('2010\Avis_20100101_20101231.xml')
+    Avis.append('2009\Avis_20090101_20091231.xml')
+    return Avis
+
+
+"""
+  Load a specific SEAO XML File and return XML Data tree
+"""
+def LoadXMLFile(filename):
+    filename = filename
+    f = codecs.open(filename, 'r', encoding='utf8')
+    str_data = f.read()
+    f.close()
+    return str_data
+
+"""
+  Function to remove special charcaters from XML Data values
+  Pass a string, get a cleaned string back
+"""
+def CleanString(str_data):
+    content_start = str_data.find('<avis>')
+    if content_start == 0:
+        content_start = str_data.find('<Avis>')
+    
+    escapes = ''.join([chr(char) for char in range(1, 32)])
+    escapes = escapes + '"'
+    replaced = " " * len(escapes)
+    t = str.maketrans(escapes,replaced)
+    
+    newstr = str_data[:content_start] + str_data[content_start:].translate(t)
+    
+    return newstr
+
+"""
+  Extracts all Avis rom the export structure in the XML Data
+  
+  The <avis></avis> tag is ususally lower case, but sometimes mistakes are introduced
+"""
+def ExtractAllAvis(xml_data):
+    all_avis = xml_data.findall('avis')
+    if (len(all_avis) == 0):
+        all_avis = xml_data.findall('Avis')
+    
+    return all_avis
+
 
 """ Load data from local folder """
-Avis = seao.InitDataPaths()
+Avis = InitDataPaths()
 
 
 """ Get first file from Avis[] array and load XML data as a string """
-#for fileIndex in range(13,len(Avis)):
-fileIndex = 8
-xml_data = seao.LoadXMLFile(Avis[fileIndex])
-print("- Avis File Loaded : " + Avis[fileIndex])
+for fileIndex in range(0, 2):
+#    fileIndex = 8
 
-all_avis = seao.ExtractAllAvis(xml_data)
-print("- Extracted all avis with fournisseurs")
+    str_data = LoadXMLFile(Avis[fileIndex])
+    print("- Avis File Loaded as String: " + Avis[fileIndex])
+    
+    str_data_cleaned = CleanString(str_data)
+    print("- XML data has been cleaned")
+    
+    xml_data = et.fromstring(str_data)
+    all_avis = ExtractAllAvis(xml_data)
+    print("XML Data items___ : " + str(len(all_avis)))
 
-small_avis = seao.RemoveFournisseurs(all_avis)
-print("- Fournisseurs striped from all loaded Avis")
+    xml_dict = xmltodict.parse(str_data_cleaned)
+    print("- XML Data converted to dictionnary")
+    json_str = json.dumps(xml_dict, indent=None)
+    print("- XML from dictionnary tranformed as JSON string")
+    json_dict = json.loads(json_str)
+    print("- JSON dictionnary loaded, ready for manipulation")
 
-str_avis = seao.GenerateJsonString(small_avis)
-print("- Generated Json File to save")
 
-saveJsonFile(Avis[fileIndex],str_avis)
+    avis_key = 'avis'
+    if avis_key not in json_dict['export']:
+        avis_key = 'Avis'
+
+    new_json = ''
+    x = 0
+    milliers = 0
+    for unavis in json_dict['export'][avis_key]:
+        unavis.pop('fournisseurs')
+        new_json = new_json + json.dumps(unavis) + chr(10)    
+        x = x + 1
+        if x > 1000:
+            x = 0
+            milliers = milliers + 1000
+            print('Generated : ' + str(milliers))
+        
+        
+    saveJsonFile(Avis[fileIndex],new_json)
+    print("- Json file saved")
+
+
+
 print("- End of execution")
 
 
